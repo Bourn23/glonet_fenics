836413bc32e1025bc31f0b82d12666e3b0b18338
برنا رهنما <brfarnood@gmail.com>
2021-01-26 18:50:33 -0500

    9

--- matlab.py
+++ matlab.py
@@ -3,8 +3,8 @@ import torch
 class engine:
     def __init__(self, fenics_model, wavelength, angle):
         self.model = fenics_model
-        self.wavelength = torch.tensor(wavelength, requires_grad = True, dtype = torch.float64)
-        self.desired_angle = torch.tensor(angle, requires_grad = True, dtype = torch.float64)
+        self.wavelength = torch.tensor([wavelength], requires_grad = True, dtype = torch.float64)
+        self.desired_angle = torch.tensor([angle], requires_grad = True, dtype = torch.float64)
         self.u = None
     def Eval_Eff_1D_parallel(self, img, wavelength, desired_angle):
         # this only works a single image
@@ -14,8 +14,8 @@ class engine:
     def GradientFromSolver_1D_parallel(self, img):
         if self.u is None:
             if type(self.wavelength) != torch.Tensor:
-                self.wavelength = torch.tensor(self.wavelength, dtype = torch.float64, requires_grad=True)
-                self.desired_angle = torch.tensor(self.desired_angle, dtype = torch.float64, requires_grad=True)
+                self.wavelength = torch.tensor([self.wavelength], dtype = torch.float64, requires_grad=True)
+                self.desired_angle = torch.tensor([self.desired_angl]e, dtype = torch.float64, requires_grad=True)
             print(self.wavelength, self.desired_angle)
             self.u = self.model(self.wavelength, self.desired_angle)
         self.u.sum().backward()
