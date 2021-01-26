7e3cf82d5cc1b918bf4365e2ac245c317142eb48
برنا رهنما <brfarnood@gmail.com>
2021-01-26 18:34:28 -0500

    m2

--- matlab.py
+++ matlab.py
@@ -1,7 +1,8 @@
 class engine:
     def __init__(self, fenics_model, *args):
-        self.model = fenics_model()
-        self.args = args
+        self.model = fenics_model
+        self.wavelength = args[0]
+        self.desired_angle = args[1]
         self.u = None
     def Eval_Eff_1D_parallel(self, img, wavelength, desired_angle):
         # this only works a single image
@@ -9,6 +10,8 @@ class engine:
         return self.u - img
     
     def GradientFromSolver_1D_parallel(self, img):
+        if self.u is None:
+            self.u = model(self.wavelength, self.desired_angle)
         self.u.sum().backward()
         effs_and_gradients = []
         effs_and_gradients.append(self.u - img)
