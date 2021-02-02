d7aae2934f03037fa3fa32364a3e6c8a37380a54
برنا رهنما <brfarnood@gmail.com>
2021-02-02 00:40:26 -0500

    36

--- train_and_evaluate.py
+++ train_and_evaluate.py
@@ -240,8 +240,8 @@ def global_loss_function(gen_imgs, effs, gradients, sigma=0.5, binary_penalty=0)
                                     # batchsize
     eff_loss_tensor = [- gen_imgs.view(10, -1) * gradients[i] for i in range(len(gradients))]
     eff_loss_tensor = sum(eff_loss_tensor).view(10, -1, 3)
-    logging.info(f'eff_loss_tensor: {eff_loss_tensor} and type is {eff_loss_tensor.size()}')
-    eff_loss_tensor *= (1./sigma) * (torch.exp(effs/sigma)).view(-1, 1, 1)
+    logging.info(f'eff_loss_tensor: {eff_loss_tensor} and type is {torch.exp(effs/sigma).size()}')
+    eff_loss_tensor *= (1./sigma) * (torch.exp(effs/sigma)).view(10, -1, 3)
     eff_loss = torch.sum(torch.mean(eff_loss_tensor, dim=0).view(-1))
 
     # binarization loss
