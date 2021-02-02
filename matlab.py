0a59c4a317a1a414290b156ff8f3a7a185af5740
برنا رهنما <brfarnood@gmail.com>
2021-02-02 00:46:48 -0500

    36

--- matlab.py
+++ matlab.py
@@ -10,11 +10,11 @@ class engine:
         self.u = None
         # logging.info(f"matlab_All initialized: type of wave {type(self.wavelength)} and angle {type(self.desired_angle)}")
     def Eval_Eff_1D_parallel(self, img, wavelength, desired_angle):
-        logging.info("matlab_eval_eff_called")
+        # logging.info("matlab_eval_eff_called")
         # this only works a single image
         if self.u is None:
             if type(self.wavelength) != torch.Tensor:
-                logging.info("matlab_EVAL_EFF_data_U_Unknown")
+                # logging.info("matlab_EVAL_EFF_data_U_Unknown")
                 self.wavelength = torch.tensor([[self.wavelength]] * self.batch_size, dtype = torch.float64, requires_grad=True)
                 self.desired_angle = torch.tensor([[self.desired_angl]] * self.batch_size, dtype = torch.float64, requires_grad=True)
             self.u = self.model(self.wavelength, self.desired_angle)
@@ -24,21 +24,21 @@ class engine:
     def GradientFromSolver_1D_parallel(self, img):
         # What should be going on here? 1. see paper for what they're doing / 2. see the .mat file
         # And what is going on now?:))
-        logging.info("matlab_grad_solver_called")
+        # logging.info("matlab_grad_solver_called")
         if self.u is None:
             if type(self.wavelength) != torch.Tensor:
-                logging.info("matlab_EVAL_EFF_data_U_Unknown_TENSORS")
+                # logging.info("matlab_EVAL_EFF_data_U_Unknown_TENSORS")
                 self.wavelength = torch.tensor([[self.wavelength]] * self.batch_size, dtype = torch.float64, requires_grad=True)
                 self.desired_angle = torch.tensor([[self.desired_angl]] * self.batch_size, dtype = torch.float64, requires_grad=True)
 
             # compute gradients for all!
             self.u = self.model(self.wavelength, self.desired_angle)
-        logging.info(f"self.u.size before is : {self.u.size()}")
+        # logging.info(f"self.u.size before is : {self.u.size()}")
         self.u.mean().backward(retain_graph = True) # mean(axis = 0) to average over batches I'm thinking how to calculate gradients for each and one of them
         # self.u.sum().backward(retain_graph = True)
 
-        logging.info(f"self.u.size is : {self.u.size()}")
-        logging.info(f"img size is : {img.size()}")
+        # logging.info(f"self.u.size is : {self.u.size()}")
+        # logging.info(f"img size is : {img.size()}")
 
         effs_and_gradients = []
         # effs_and_gradients.append(self.u[:, :100, 1] - img[:, :, torch.randint(0, 255, (1,))[0].numpy().tolist()]) # dims needs to match
