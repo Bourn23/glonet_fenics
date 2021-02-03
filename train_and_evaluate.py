72dc314b88727e2573d86807feac76a8913ba780
برنا رهنما <brfarnood@gmail.com>
2021-02-02 22:17:54 -0500

    42

--- train_and_evaluate.py
+++ train_and_evaluate.py
@@ -275,32 +275,43 @@ def save_images(imgs, eng, fig_path):
         yaxis = dict(range=[-1, 1], showbackground=False, zerolinecolor="black"),
         zaxis = dict(range=[-1, 1], showbackground=False, zerolinecolor="black"))
 
-    x, y, z = eng.model.mesh.coordinates().T
-    fig = go.Figure(data=[go.Scatter3d(x=x, y=y, z=z,
-                                    mode='markers')])
-    fig.update_layout(scene = scene_settings)
-    fig.update_layout(scene_aspectmode = 'cube')
-    # fig.show()
-    fig.write_image(fig_path)
-    # scene_settings = dict(
-    #         xaxis = dict(range=[-2, 2], showbackground=False, zerolinecolor="black"),
-    #         yaxis = dict(range=[-1, 1], showbackground=False, zerolinecolor="black"),
-    #         zaxis = dict(range=[-1, 1], showbackground=False, zerolinecolor="black"))
 
-    # triangles = []
-    # for cell in cells(eng.model.mesh):
-    #     for facet in facets(cell):
-    #         vertex_coords = []
-    #         vertex_indices = []
-    #         for vertex in vertices(facet):
-    #             vertex_coords.append(list(vertex.point().array()))
-    #             vertex_indices.append(vertex.index())
-    #         triangles.append(vertex_indices)
+    triangles = []
+    for cell in cells(eng.model.mesh):
+        for facet in facets(cell):
+            vertex_coords = []
+            vertex_indices = []
+            for vertex in vertices(facet):
+                vertex_coords.append(list(vertex.point().array()))
+                vertex_indices.append(vertex.index())
+            triangles.append(vertex_indices)
     
-    # tris = np.array(triangles)
+    tris = np.array(triangles)
+    
+    x, y, z = (varproblem.mesh.coordinates() + imgs.detach().numpy()).T
+    i, j, k = tris.T
+    disp = np.linalg.norm(imgs.detach().numpy(), axis=1).T
+    fig = go.Figure(data=[
+        go.Mesh3d(
+            x=x,
+            y=y,
+            z=z,
+            # Intensity of each vertex, which will be interpolated and color-coded
+            intensity=disp,
+            # i, j and k give the vertices of triangles
+            # here we represent the 4 triangles of the tetrahedron surface
+            i=i,
+            j=j,
+            k=k,
+            name='y',
+            showscale=True
+        )
+    ])
+    fig.update_layout(scene = scene_settings)
+    # fig.update_layout(scene_aspectmode = 'cube')
+    fig.show()
 
-    # x, y, z = (eng.model.mesh.coordinates() + imgs.detach().numpy()[0]).T
-    # i, j, k = tris.T
+    
     # # [0] change it to i so to save all images!
     # disp = np.linalg.norm(imgs.detach().numpy(), axis=1).T  # the zero index is because of the "N" above!
 
