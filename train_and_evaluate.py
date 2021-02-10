ff3ca8f2f2bb689880809161659d124afed1cc9b
برنا رهنما <brfarnood@gmail.com>
2021-02-10 12:35:50 -0500

    53

--- train_and_evaluate.py
+++ train_and_evaluate.py
@@ -231,7 +231,7 @@ def global_loss_function(gen_imgs, effs, gradients, sigma=0.5, binary_penalty=0)
     gradients =  gradients.squeeze(2).T.unsqueeze(2).repeat(1, 88, 3)
     difference = torch.sum(torch.mean(effs - gen_imgs, dim=2), dim=1)
     eff_loss_tensor = - gen_imgs * gradients * (1./sigma) * (torch.exp(difference/sigma)).view(-1, 1, 1)
-    logging.info((1./sigma) * (torch.exp(difference/sigma))
+    logging.info((1./sigma) * (torch.exp(difference/sigma)))
     eff_loss = torch.sum(torch.mean(eff_loss_tensor, dim=0).view(-1))
 
     actual_fft = torch.fft.fft(effs)
