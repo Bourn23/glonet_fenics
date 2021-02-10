40c8fbc03a98b573b122ccec9898cedbeacef188
برنا رهنما <brfarnood@gmail.com>
2021-02-10 12:31:50 -0500

    53

--- train_and_evaluate.py
+++ train_and_evaluate.py
@@ -228,22 +228,14 @@ def global_loss_function(gen_imgs, effs, gradients, sigma=0.5, binary_penalty=0)
     '''
 
     # efficiency loss
-    logging.info(gen_imgs.size())
-    node_per_axis = 176
-    axis = 3
-    repeat_nodes = int(node_per_axis / gradients.shape[2])
-    logging.info(f'{repeat_nodes}')
-    gradients =  gradients.squeeze(2).T.unsqueeze(2).repeat(1, repeat_nodes, axis)
+    gradients =  gradients.squeeze(2).T.unsqueeze(2).repeat(1, 88, 3)
     difference = torch.sum(torch.mean(effs - gen_imgs, dim=2), dim=1)
-    logging.info(f"shape of difference is {difference.shape}")
     eff_loss_tensor = - gen_imgs * gradients * (1./sigma) * (torch.exp(difference/sigma)).view(-1, 1, 1)
     eff_loss = torch.sum(torch.mean(eff_loss_tensor, dim=0).view(-1))
 
     actual_fft = torch.fft.fft(effs)
     pred_fft = torch.fft.fft(gen_imgs)
     fft_loss = torch.square(torch.real(actual_fft-pred_fft))
-
-    
 
     loss = eff_loss + fft_loss #+ time_loss
     return loss
