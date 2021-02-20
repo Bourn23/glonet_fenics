1e9e4ded4fbb4cf131a5a9e5f9ed95945898021c
برنا رهنما <brfarnood@gmail.com>
2021-02-20 12:35:18 -0500

    12

--- net.py
+++ net.py
@@ -12,8 +12,8 @@ class Generator:
     def __init__(self, params):
 
         # self.beta = torch.randn(1, 1, requires_grad = True, dtype = torch.float64)
-        self.mu = torch.DoubleTensor(params.batch_size_start, 1).uniform_(0., params.mu + torch.rand(1)[0]).requires_grad_(True)
-        self.beta = torch.DoubleTensor(params.batch_size_start, 1).uniform_(0., params.mu + torch.rand(1)[0]).requires_grad_(True)
+        self.mu = torch.DoubleTensor(params.batch_size_start, 1).uniform_(0., params.mu+torch.rand(1)[0]*10).requires_grad_(True)
+        self.beta = torch.DoubleTensor(params.batch_size_start, 1).uniform_(0., params.mu+torch.rand(1)[0]*10).requires_grad_(True)
         self.force = torch.DoubleTensor([[params.force]] * params.batch_size_start)#, ruquires_grad = True)
         self.params = [self.mu, self.beta]
     
