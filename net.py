58d871eb29d4202aeba783c8d422c4bf86dc4d1a
borna <brfarnood@gmail.com>
2021-03-07 11:59:55 -0500

    1

--- net.py
+++ net.py
@@ -190,14 +190,14 @@ class SGD(Model):
 
         # ax.contourf(X, Y, Z.reshape(X.shape)) # these are gaussian models' values
         ax[0].set_title('history of mu and beta')
-        ax[0].plot(self.history[:, 0], self.history[:, 1], 'rx')  # values obtained by torch
-        ax[0].plot(self.generator.E_0, self.generator.nu_0, 'ws')  # white = true value
+        ax[0].plot(self.history[:, 0], self.history[:, 1], '-x')  # values obtained by torch
+        ax[0].plot(self.generator.E_0, self.generator.nu_0, 'gs')  # white = true value
 
 
         # ax.contourf(X, Y, Z.reshape(X.shape))
         ax[0].set_title('history of E_0 and nu_0')
-        ax[1].plot(self.data[:, 0], self.data[:, 1], 'rx')  # values obtained by torch
-        ax[1].plot(self.generator.E_0, self.generator.nu_0, 'ws')  # white = true value
+        ax[1].plot(self.data[:, 0], self.data[:, 1], '-x')  # values obtained by torch
+        ax[1].plot(self.generator.E_0, self.generator.nu_0, 'gs')  # white = true value
 
 
         plt.savefig(fig_path, dpi = 300)
