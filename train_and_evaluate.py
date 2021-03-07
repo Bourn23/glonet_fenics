10ef4d7f8ecb09a161398560d493c257c0db0884
borna <brfarnood@gmail.com>
2021-03-07 11:51:08 -0500

    1

--- train_and_evaluate.py
+++ train_and_evaluate.py
@@ -94,7 +94,7 @@ def train(eng, params, pca=None):
     
     # training loop
     # with tqdm(params.numIter, desc = "sub_optimizers", leave = False) as t:
-    for _ in tqdm(total = params.numIter, desc = "sub_optimizers", leave = False):
+    for _ in tqdm(range(params.numIter), desc = "sub_optimizers", leave = False):
         t=0
 
         it = 0  
