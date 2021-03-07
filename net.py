c9b75bdb2d5dfeda4069ba85bab88c77d9d1473b
borna <brfarnood@gmail.com>
2021-03-07 05:56:02 -0500

    3

--- net.py
+++ net.py
@@ -258,7 +258,7 @@ class SGD(Model):
         self.optimizer.zero_grad()
         err = torch.log(loss(pred_deflection, eng.target_deflection))
         err.backward()
-        self.history = np.vstack([self.history, np.array([self.mu.detach(), self.beta.detach(), self.err.detach()])])  
+        self.history = np.vstack([self.history, np.array([self.mu.detach(), self.beta.detach(), err.detach()])])  
 
 
 
