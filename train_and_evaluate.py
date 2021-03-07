893e63c0945ccb4276ffffd052b14807877cb1cc
borna <brfarnood@gmail.com>
2021-03-07 10:25:43 -0500

    1

--- train_and_evaluate.py
+++ train_and_evaluate.py
@@ -77,7 +77,7 @@ def train(eng, params, pca=None):
         
 
         if model_param: exec(f"{model} = {name}(model_params, eng)") #Init with params
-        else: print('is this running') ;         exec(f"active = {name}(params, eng)"); active_models[f'{model}'] = active
+        else: print('is this running') ;         exec(f"ac = {name}(params, eng)"); active_models[f'{model}'] = ac
         
         # experiment with having exec insantiate the class; how else I can insantiate?
     print(active_models)
