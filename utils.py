835d877083263e6e2a71949526827c8736da96e5
borna <brfarnood@gmail.com>
2021-03-15 19:56:46 -0400

    2

--- utils.py
+++ utils.py
@@ -136,7 +136,7 @@ def load_checkpoint(checkpoint, model, optimizer=None, scheduler=None):
 
 def plot_loss_history(params, active_models, global_memory):
     path = params.output_dir + f'/figures/ensemble_of_decision_1.png'
-    fig, ax = plt.subplots(1, len(active_models), figsize = (6, 4))
+    fig, ax = plt.subplots(1, len(active_models)+1, figsize = (6, 4))
     counter = 0
     for name, model in active_models.items():
         E_history, nu_history, eff_history = model.data[:, 0], model.data[:, 1], model.data[:, 2]
