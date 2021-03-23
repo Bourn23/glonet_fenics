543204e5b522f6e27993bbd03f3c7705194a2bc6
borna <brfarnood@gmail.com>
2021-03-22 21:15:45 -0400

    1

--- net.py
+++ net.py
@@ -439,10 +439,11 @@ class GA(Model):
             E_f, nu_f = lame(data[0]*1e8, data[1])
 
             # check dimensions
-            E_coeff = round(math.log(self.generator.E_0, 10) - round(math.log(E_f, 10), 0), 0)
-            nu_coeff = round(math.log(self.generator.nu_0, 10) - round(math.log(E_f, 10), 0), 0)
+            # E_coeff = round(math.log(self.generator.E_0, 10) - round(math.log(E_f, 10), 0), 0)
+            # nu_coeff = round(math.log(self.generator.nu_0, 10) - round(math.log(E_f, 10), 0), 0)
+            # data = {'mu': E_f* 10**E_coeff, 'beta':nu_f* 10**nu_coeff}
 
-            data = {'mu': E_f* 10**E_coeff, 'beta':nu_f* 10**nu_coeff}
+            data = {'mu': E_f, 'beta':nu_f}
             print('data is ', data)
 
             if (data['mu'] <= 0) or (data['beta'] <= 0): # penalize invalid values
