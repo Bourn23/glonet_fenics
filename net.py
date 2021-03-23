5a97db160a32ad11e27a22a09457aec3c6ec0251
borna <brfarnood@gmail.com>
2021-03-22 20:52:43 -0400

    1

--- net.py
+++ net.py
@@ -518,8 +518,8 @@ class GA(Model):
         print(f'after young poisson: E is {E_f}, f is{nu_f}')
 
         # scale the size
-        E_f_coef = math.floor(abs(math.log(self.generator.E_0, 10) - math.log(E_f, 10)))
-        nu_f_coef = math.floor(abs(math.log(self.generator.nu_0, 10) - math.log(nu_f, 10)))
+        E_f_coef = math.floor(math.log(self.generator.E_0, 10) - math.log(E_f, 10))
+        nu_f_coef = math.floor(math.log(self.generator.nu_0, 10) - math.log(nu_f, 10))
 
         print('inverted values: {:.2e} {:.2e}'.format(E_f* 10**E_f_coef, nu_f* 10**nu_f_coef))
         print('error:           {:7.2f}% {:7.2f}%'.format((E_f* 10**E_f_coef-self.generator.E_0)/self.generator.E_0*100,
