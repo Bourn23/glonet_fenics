2d607b01d7a5b5f173aaae40151f967f1f00188b
borna <brfarnood@gmail.com>
2021-03-25 00:21:38 -0400

    plotting

--- matlab.py
+++ matlab.py
@@ -164,7 +164,7 @@ class engine:
             a_ = torch.tensor([i for i in range(mu.shape[0]) if i % 2 == 0])
             b_ = torch.tensor([i for i in range(mu.shape[0]) if i % 2 == 1])
             inds1 = torch.cat([a_.view(2, -1), b_.view(2, -1)])
-            b = torch.tensor([1,2]).repeat(len(mu.shape[0]//2))
+            b = torch.tensor([1,2]).repeat(mu.shape[0]//2)
             _, inds2 = torch.sort(b)
             output = output[1:,:].flatten()[inds1][idns2].numpy() # 441,1
             print('final shape: ', output.shape)
