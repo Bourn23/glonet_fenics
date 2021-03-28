44d90a533ba71a2d8b5be82e199048ee0f482999
borna <brfarnood@gmail.com>
2021-03-28 14:42:39 -0400

    inhomo

--- utils.py
+++ utils.py
@@ -11,6 +11,8 @@ import scipy.io as io
 import torch
 import numpy as np
 
+from dolfin import *; from mshr import *
+
 
 import glob
 import shutil
@@ -312,7 +314,7 @@ def plot_3d(eng, particles=None, velocity=None, normalize=True, color='#000', ax
 def save_images(imgs, eng, fig_path):
     import plotly.graph_objects as go
     import numpy as np
-    from dolfin import *; from mshr import *
+    
     v2d = vertex_to_dof_map(eng.model.V)
 
     imgs = imgs[0].flatten()[v2d].reshape(-1, 3)# / 10.#0.
