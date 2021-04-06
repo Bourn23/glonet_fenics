a3ccb3cd02248c992c32f988da0bc1f862065aa1
borna <brfarnood@gmail.com>
2021-04-06 00:29:41 -0400

    plot3d

--- utils.py
+++ utils.py
@@ -291,7 +291,8 @@ def plot_3d(eng, particles=None, velocity=None, normalize=True, color='#000', ax
             y = y[:,::-1]
             z = z[:,::-1]
             fig = go.Figure(data=[go.Surface(z=z, x=x, y=y)])
-            fig.add_scatter(z=[z.min()], x=[x[x_min]], y=[y[y_min]])
+            fig.add_scatter(, mode = 'marker')
+            fig.add_traces([go.Scatter3D(z=[z.min()], x=[x[x_min]], y=[y[y_min]])]) 
             # , go.Scatter3d(z = , x = , y = )])
             # fig = go.Figure(data=[go.Surface(z=z[:,::-1], x=x, y=y[:,::-1]), go.Scatter3d(z = z.min(), x = x[x_min], y = y[y_min])])
             fig.update_layout(scene = dict(
